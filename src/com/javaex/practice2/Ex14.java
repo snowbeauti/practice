a13d2f71f44991ea1f30ac039b7c98a99d523f18
snowbeauti <snowsaj119@gmail.com>
2020-11-13 18:00:35 +0900

    14번 수정

--- src/com/javaex/practice2/Ex14.java
+++ src/com/javaex/practice2/Ex14.java
@@ -21,17 +21,21 @@ public class Ex14 {
 		lev3 = 0.27;
 		lev4 = 0.36;
 		
+		long ran1, ran2, ran3;
+		ran1 = 1000;
+		ran2 = 4000;
+		ran3 = 8000;
+		
 		double tax;
 		
 		if(0 <= salary) { 			
-		if(0 <= salary && salary<=1000) {
+		if(0 <= salary && salary<=ran1) {
 			tax = salary * lev1;
-		} else if (1000 < salary && salary<=4000) {
-			tax = 1000 * lev1 + (salary-1000) * lev2;
-		} else if (4000 < salary && salary<=8000) {
-			tax = 1000 * lev1 + 3000 * lev2 + (salary-4000) * lev3;
-		} else  {
-			tax = 1000 * lev1 + 3000 * lev2 + 4000 * lev3 + (salary-8000) * lev4;
+		} else if (ran1 < salary && salary<=ran2) {
+			tax = ran1 * lev1 + (salary-ran1) * lev2;
+		} else if (ran2 < salary && salary<=ran3) {
+			tax = ran1 * lev1 + (ran2 - ran1) * lev2 + (salary - ran2) * lev3;
+		} else  {tax = ran1 * lev1 + (ran2 - ran1) * lev2 + (ran3 - ran2) * lev3 + (salary - ran3) * lev4;
 		} System.out.print("소득세는 " + tax + "입니다.");
 		} else { System.out.print("잘못 입력했습니다.");}
 		
